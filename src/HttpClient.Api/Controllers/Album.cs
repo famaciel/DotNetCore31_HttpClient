738279c0c30fa7d75108af4b30947a483d5c340d
Fernando Maciel <fernando.maciel80@gmail.com>
2021-11-26 16:11:51 -0300

    Add Album Controller

---
+++ src/HttpClient.Api/Controllers/Album.cs
@@ -0,0 +1,78 @@
+﻿using HttpClient.Api.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Mime;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace HttpClient.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Album : ControllerBase
+    {
+        private readonly IHttpClientFactory _clientFactory;
+
+        public Album(IHttpClientFactory clientFactory)
+        {
+            _clientFactory = clientFactory;
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(AlbumModel))]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> Get(int id)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, $"https://jsonplaceholder.typicode.com/albums/{id}");
+
+            var client = _clientFactory.CreateClient();
+
+            var response = await client.SendAsync(request);
+
+            if (response.IsSuccessStatusCode)
+            {
+                using var responseStream = await response.Content.ReadAsStreamAsync();
+                var album = await JsonSerializer.DeserializeAsync<AlbumModel>(responseStream);
+
+                return Ok(album);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPost]
+        [Consumes(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(AlbumModel))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Post([FromBody] AlbumModel value)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var albumContent = new StringContent(JsonSerializer.Serialize(value), Encoding.UTF8, "application/json");
+
+            using var response = await _clientFactory.CreateClient().PostAsync("https://jsonplaceholder.typicode.com/albums", albumContent);
+
+            if (response.IsSuccessStatusCode)
+            {
+                using var responseStream = await response.Content.ReadAsStreamAsync();
+                var result = await JsonSerializer.DeserializeAsync<AlbumModel>(responseStream);
+
+                return CreatedAtAction(nameof(Get), new { id = result.id }, result);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
--- src/HttpClient.Api/Models/AlbumModel.cs
+++ src/HttpClient.Api/Models/AlbumModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,11 @@ namespace HttpClient.Api.Models
 {
     public class AlbumModel
     {
+        [Required]
+        [Range(1, int.MaxValue)]
         public int userId { get; set; }
         public int id { get; set; }
+        [Required]
         public string title { get; set; }
     }
 }
